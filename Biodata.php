0cdf77c4939d5d2675083b16a092996379e8fc88
Santy <santyrahmawatiusman@gmail.com>
2025-04-27 20:42:16 +0700

    Menambahkan Biodata.php

---
+++ Biodata.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Biodata Saya</h1>
+    <div class="container">
+        <table>
+            <tr><th>Nama</th><td>Santy Rahmawati Usman</td></tr>
+            <tr><th>Kelas</th><td>IF D 2023</td></tr>
+            <tr><th>NPM</th><td>5520123143</td></tr>
+            <tr><th>Praktikum</th><td>PWD-8</td></tr>
+            <tr><th>Tugas Mandiri</th><td>6 (Materi PHP Lanjutan)</td></tr>
+        </table>
+    </div>
+</body>
+</html>

f543187b349f0e7d2a381340c4480f8465b93796
Santy <santyrahmawatiusman@gmail.com>
2025-04-27 20:41:25 +0700

    Menambahkan Soal2.php Perkalian Matriks

---
+++ Soal2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Soal 2: Tabel Perkalian 1-10</h1>
+    <div class="container">
+        <table>
+            <tr>
+                <th>Bilangan</th>
+                <?php
+                    for ($i = 1; $i <= 10; $i++) {
+                        echo "<th>$i</th>";
+                    }
+                ?>
+            </tr>
+            <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<tr>";
+                    echo "<th>$i</th>";
+                    for ($j = 1; $j <= 10; $j++) {
+                        $hasil = $i * $j;
+                        $warna = ($i == 1 || $j == 1) ? 'kuning' : 'biru';
+                        echo "<td class='$warna'>$hasil</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>

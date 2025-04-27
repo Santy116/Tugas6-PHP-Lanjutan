43bda7ff48ee1f42bbeb128450c13a17526604de
Santy <santyrahmawatiusman@gmail.com>
2025-04-27 20:38:08 +0700

    Menambahkan File Pertama: Soal 1

---
+++ Soal1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Soal 1: Menampilkan Bilangan Genap 1-10</h1>
+    <div class="container">
+        <?php
+            for ($i = 1; $i <= 10; $i++) {
+                if ($i % 2 == 0) {
+                    echo "<span class='genap'>$i </span>";
+                }
+            }
+        ?>
+    </div>
+</body>
+</html>
